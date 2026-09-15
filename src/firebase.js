d24bc0e94f79b3c0a0d87a1048572f1b4bf571d0
David <david@Admins-MacBook-Pro.local>
2026-09-15 18:15:37 -0500

    Update firebase.js with incentive-vault-final keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPCvv4m-ZNLEz0Y_s0u9QleQimUCS_CUY",
-  authDomain: "incentive-vault-2.firebaseapp.com",
-  projectId: "incentive-vault-2",
-  storageBucket: "incentive-vault-2.firebasestorage.app",
-  messagingSenderId: "462037126432",
-  appId: "1:462037126432:web:dc4f244a246986e499b45",
+  apiKey: "AIzaSyAbXHqRRl-HIZezcCXr6WIqs_toCxKWZ_c",
+  authDomain: "incentive-vault-final.firebaseapp.com",
+  projectId: "incentive-vault-final",
+  storageBucket: "incentive-vault-final.firebasestorage.app",
+  messagingSenderId: "378781635243",
+  appId: "1:378781635243:web:f73b7b8c7931149f2dddee",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
