aa67b9a66c6c6126881bf5091508a9785a2eeff2
David <david@MacBookPro.lan>
2026-09-17 14:33:02 -0500

    Update Dashboard to display barn name instead of registered name

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -66,9 +66,9 @@ function Dashboard() {
             <div className="horses-grid">
               {horses.map(horse => (
                 <div key={horse.id} className="horse-card">
-                  {horse.photo && <img src={horse.photo} alt={horse.name} />}
+                  {horse.photo && <img src={horse.photo} alt={horse.barnName} />}
                   <div className="horse-info">
-                    <h3>{horse.name}</h3>
+                    <h3>{horse.barnName}</h3>
                     <p className="horse-meta">
                       {horse.sire && <span>By {horse.sire}</span>}
                       {horse.age && <span>{horse.age} yrs</span>}
